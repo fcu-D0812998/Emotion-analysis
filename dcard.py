3c9baea376ffe46d56058916dd5ea8046f65cbb2
fcu-d0812998 <72053280+fcu-D0812998@users.noreply.github.com>
2022-01-10 21:15:42 +0800

    first commit

--- dcard.py
+++ dcard.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-url = 'https://www.dcard.tw/f/nba'
+url = 'https://www.dcard.tw/f'
 resp = requests.get(url)
 soup = BeautifulSoup(resp.text,'html.parser')
 decard_title = soup.find_all('hs',re.compile(()))
